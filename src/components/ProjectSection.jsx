86c19a2b3f28281f6235f8c9d9e68943c6d5824d
Zachary Casares <rrumble250@gmail.com>
2023-09-04 19:25:30 -0400

    create projects section

---
+++ src/components/ProjectSection.jsx
@@ -0,0 +1,12 @@
+import Project from "./Project";
+import Class from "./ProjectSection.module.css";
+
+function Projects() {
+  return (
+    <div className={Class.container}>
+      <Project />
+    </div>
+  );
+}
+
+export default Projects;
