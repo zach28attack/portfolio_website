90e8c9617fa79df251c6c89f3a69264173e6c49c
Zachary Casares <rrumble250@gmail.com>
2023-09-04 19:24:11 -0400

    add smooth scrolling to projects button

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import GreetingCard from "./components/GreetingCard";
 import BGVideo from "./components/BGVideo";
 import About from "./components/About";
 import {useRef} from "react";
+import ProjectSection from "./components/ProjectSection";
 
 function App() {
-  const sectionRef = useRef(null);
+  const projectsRef = useRef(null);
+  const aboutRef = useRef(null);
   return (
     <>
-      <GreetingCard sectionRef={sectionRef} />
+      <GreetingCard projectsRef={projectsRef} aboutRef={aboutRef} />
       <BGVideo />
-      <div ref={sectionRef}>
+      <div ref={projectsRef}>
+        <ProjectSection />
+      </div>
+      <div ref={aboutRef}>
         <About />
       </div>
     </>
