d669118dac51292cc795f9fc0a5dba3e5db07905
Zachary Casares <rrumble250@gmail.com>
2023-09-04 13:29:57 -0400

    add links

--- src/components/GreetingCard.jsx
+++ src/components/GreetingCard.jsx
@@ -5,6 +5,12 @@ import SkillIcons from "./SkillIcons";
 function GreetingCard() {
   return (
     <div className={Class.container}>
+      <div className={Class.navbar}>
+        <span>About</span>
+        <span>Contact</span>
+        <span>Resume</span>
+      </div>
+
       <section className={Class.nameGroup}>
         <div className={Class.name}>
           <h1>Zachary Casares</h1>
