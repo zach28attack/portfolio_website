d7483c08803d1b100e89e991c27c9d18ffa2ef62
Zachary Casares <rrumble250@gmail.com>
2023-10-01 21:29:17 -0400

    prevent ios devices from showing play button

--- src/components/BGVideo.jsx
+++ src/components/BGVideo.jsx
@@ -1,8 +1,20 @@
 import Class from "./BGVideo.module.css";
+import {useRef} from "react";
 
 function BGVideo() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    video.play().catch((error) => {
+      // Handle any errors that might occur when attempting to autoplay
+      console.error("Autoplay failed:", error);
+    });
+  }, []);
+
   return (
     <video
+      ref={videoRef}
       className={Class.video}
       loop
       autoPlay
