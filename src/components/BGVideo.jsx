f5addd5a832f208acff076b0110f38bbdd594e61
Zachary Casares <rrumble250@gmail.com>
2024-01-02 22:07:39 -0500

    remove dead code

--- src/components/BGVideo.jsx
+++ src/components/BGVideo.jsx
@@ -6,23 +6,10 @@ function BGVideo({setIsVideoLoaded}) {
 
   useEffect(() => {
     setIsVideoLoaded(true);
-    const video = videoRef.current;
-    video.play().catch((error) => {
-      // Handle any errors that might occur when attempting to autoplay
-      console.error("Autoplay failed:", error);
-    });
   }, []);
 
   return (
-    <video
-      ref={videoRef}
-      className={Class.video}
-      loop
-      controlsList="nodownload"
-      preload="auto"
-      muted="muted"
-      poster="/Untitled.mp4"
-    >
+    <video ref={videoRef} className={Class.video} controlsList="nodownload" muted="muted" poster="/Untitled.mp4">
       <source src="/Untitled.mp4" type="video/mp4" />
     </video>
   );
